7dbac96bb975692b0a7ed4e9e4fd0b2e1032ab8c
Ishwar Choudhary <ishwar@github.com>
2020-03-01 15:13:07 +0530

    readded flutter

---
+++ village_mgmt_flutterapp/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() =>
+    runApp(MaterialApp(title: "Village Management", home: MainActivity()));
+
+class MainActivity extends StatefulWidget {
+  @override
+  _MainActivityState createState() => _MainActivityState();
+}
+
+class _MainActivityState extends State {
+  String msg = 'CLICK ON BUTTON TO GET CURRENT STATUS';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Village Management'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text("Water Supply", style: TextStyle(fontSize: 30)),
+                onPressed: _getWaterStatus,
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                child:
+                    Text("Electricity Supply", style: TextStyle(fontSize: 30)),
+                onPressed: _getElectricityStatus,
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text("Ration Delivery", style: TextStyle(fontSize: 30)),
+                onPressed: _getRationStatus,
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text("Weather", style: TextStyle(fontSize: 30)),
+                onPressed: _getRationStatus,
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+              ),
+              // Image(image: AssetImage('assets/water.png'));
+              SizedBox(height: 40),
+              Container(
+                margin: const EdgeInsets.all(30.0),
+                padding: const EdgeInsets.all(10.0),
+                decoration: myBoxDecoration(),
+                child: Column(children: [
+                  Text(
+                    msg,
+                    style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _getWaterStatus() async {
+    var url = 'https://api.myjson.com/bins/7hf4q';
+    var response = await http.get(url);
+    print('Response body: ${response.body}');
+    var data = json.decode(response.body);
+    print(data["resource"]);
+    setState(() {
+      msg = data["resource"] +"\n" +data["status"]+"\n" +data["time"];
+    });
+  }
+
+  _getElectricityStatus() async {
+    var url = 'https://api.myjson.com/bins/7hf4q';
+
+    var response = await http.get(url);
+    print('Response body: ${response.body}');
+    var data = json.decode(response.body);
+    setState(() {
+      msg = data["resource"] +"\n" +data["status"]+"\n" +data["time"];   
+       });
+  }
+
+  _getRationStatus() async {
+    var url = 'https://api.myjson.com/bins/7hf4q';
+    var response = await http.get(url);
+    print('Response body: ${response.body}');
+    var data = json.decode(response.body);
+    setState(() {
+      msg = data["resource"] +"\n" +data["status"]+"\n" +data["time"];   
+      });
+  }
+}
+
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(),
+  );
+}
